ee62a6343e66cf5a524c53250f26e34e4650b7a8
Komodoro <j_diniz1@msn.com>
2017-02-24 00:03:53 +0000

    added folders again
    after adding project

---
+++ sporto/index.php
@@ -0,0 +1,170 @@
+
+<!DOCTYPE html>
+<html lang="pt">
+  <head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sporto</title>
+    <meta name="description" content="Empresa de Eventos Desportivos">
+    <meta name="keywords" content="sporto, eventos, desporto, corrida, trail, running,">
+    <meta name="author" content="Pedro Pereira, sporto.com">
+    <meta property="og:image" content="" />
+	<meta property="og:description" content="Website da Empresa Sporto. Sporto é a página de Eventos Desportivos para Si" />
+	<meta property="og:url"content="" />
+	<meta property="og:title" content="Sporto" />
+
+    
+    <!-- Favicons
+    ================================================== -->
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css"  href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.2.0/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="css/jasny-bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+    <!-- Slider
+    ================================================== -->
+    <link href="css/owl.carousel.css" rel="stylesheet" media="screen">
+    <link href="css/owl.theme.css" rel="stylesheet" media="screen">
+
+    <!-- Stylesheet
+    ================================================== -->
+    <link rel="stylesheet" type="text/css"  href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <script type="text/javascript" src="js/modernizr.custom.js"></script>
+  </head>
+  <body>
+  	
+  	<!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+               
+                <a class="navbar-brand" href="#">Sporto</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+		            <li><a href="#home" class="page-scroll">Home</a></li>
+				    <li><a href="#eventos" class="page-scroll">Eventos</a></li>
+				    <li><a href="login.php" class="page-scroll">Login</a></li>
+				    <li><a href="register.php" class="page-scroll">Registo</a></li>
+					
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    
+
+    <!-- Home Section -->
+    <div id="home">
+        <div class="container text-center">
+            <div class="content">
+                <h4 style="color:black">Sporto</h4>
+                <hr>
+                <div class="header-text btn">
+                    <h1><span style="color:black" id="head-title"></span></h1>
+                </div>
+            </div>
+        </div>
+    </div>	
+    
+    <!-- Services Section -->
+    <div id="eventos">
+	
+		
+        <div class="container text-center">
+            
+			<div class="section-title">
+				<h2>Eventos</h2>
+				<hr>
+			</div>
+            <div class="row">
+                <div class="col-md-4 col-sm-4">
+                    <div class="service">
+                        
+                        <h4>Missão</h4>
+                        <p>A missão da Car 4 Day é desenvolver serviços para melhorar a deslocação de cada indivíduo no seu dia-a-dia, bem como melhorar esse serviço, assegurando a satisfação dos seus clientes, criando valor para os acionistas e satisfação dos seus parceiros. 
+                        	</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-4">
+                    <div class="service">
+                       
+                        <h4>Visão</h4>
+                        <p>Car 4 Day quer ser conhecida por ser uma empresa inovadora e criativa para o mercado, tentando encontrar novas necessidades e melhorias no que diz respeito ao deslocamento/mobilidade urbana.
+                        	</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-4">
+                    <div class="service">
+                       
+                        <h4>Valores</h4>
+                        <p>Integridade, cooperação, respeito pelo meio ambiente, responsabilidade social, valorização e respeito pelas pessoas, satisfação do cliente, excelência e simplicidade.  </p>
+						<p> </p>                    
+                    	<p> </p>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+    
+
+    
+
+       
+
+    <!-- Footer -->
+    
+    <nav id="footer">
+        <div class="container">
+             <div class="pull-left">
+                <p>2017 © Sporto. All Rights Reserved.</p>
+            </div>
+            <div class="pull-right"> 
+                <a href="#home" class="page-scroll">Back to Top <span class="fa fa-angle-up"></span></a>
+            </div>
+        </div>
+    </nav>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/SmoothScroll.js"></script>
+    <script type="text/javascript" src="js/jasny-bootstrap.min.js"></script>
+
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/typed.js"></script>
+    <script>
+      $(function(){
+          $("#head-title").typed({
+            strings: ["Bem-vindo a Sporto^1000", "Eventos desportivos a pensar em si^1000"],
+            typeSpeed: 100,
+            loop: true,
+            startDelay: 100
+          });
+      });
+    </script>
+    <!-- Javascripts
+    ================================================== -->
+    <script type="text/javascript" src="js/main.js"></script>
+  </body>
+</html>
